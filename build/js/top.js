8f24d7505e010345b7696df109eeb5d2bf97362e
Ricardo <ricardojuarez@gmail.com>
2022-09-04 23:11:24 -0500

    se agrego el toTop

---
+++ build/js/top.js
@@ -0,0 +1,2 @@
+const buttonTop=document.querySelector("#toTop");buttonTop.addEventListener("click",(function(){window.scrollTo({top:0,left:0,behavior:"smooth"})}));
+//# sourceMappingURL=top.js.map
